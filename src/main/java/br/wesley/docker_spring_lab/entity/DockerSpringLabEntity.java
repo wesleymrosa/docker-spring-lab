fc183fae97adebdb5abb3b6f7683395e0a0fcae4
Wesley Rosa <wesleymrosa@gmail.com>
2025-04-16 17:15:42 -0300

    Criada a entidade.

---
+++ src/main/java/br/wesley/docker_spring_lab/entity/DockerSpringLabEntity.java
@@ -0,0 +1,71 @@
+package br.wesley.docker_spring_lab.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tabela")
+public class DockerSpringLabEntity implements Serializable {
+    private static final long serializable = 1L;
+    private Long id;
+    private String nome;
+    private Integer idade;
+
+    public DockerSpringLabEntity() {
+    }
+
+    public DockerSpringLabEntity(Long id, String nome, Integer idade) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DockerSpringLabEntity that = (DockerSpringLabEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(nome, that.nome) && Objects.equals(idade, that.idade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, idade);
+    }
+
+    @Override
+    public String toString() {
+        return "DockerSpringLabEntity{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
+}
