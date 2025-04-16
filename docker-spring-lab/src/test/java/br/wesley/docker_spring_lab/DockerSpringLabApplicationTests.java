e9cf5d38acd6314e8fb89ad668317492977734c0
Wesley Rosa <wesleymrosa@gmail.com>
2025-04-16 16:47:12 -0300

    Primeiro commit

---
+++ docker-spring-lab/src/test/java/br/wesley/docker_spring_lab/DockerSpringLabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.wesley.docker_spring_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/br/wesley/docker_spring_lab/DockerSpringLabApplication.java
@@ -0,0 +1,13 @@
+package br.wesley.docker_spring_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringLabApplication.class, args);
+	}
+
+}
